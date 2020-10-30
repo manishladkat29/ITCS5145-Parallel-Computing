e598ad3010b27e14be38f4e21167a6fd412029cb
Manish Anil Ladkat <mladkat@mba-i1.uncc.edu>
2020-10-30 01:19:20 -0400

    results of reduce

--- assignment-canvas-3/reduce/reduce.cpp
+++ assignment-canvas-3/reduce/reduce.cpp
@@ -42,24 +42,21 @@ int main (int argc, char* argv[]) {
     int n = atoi(argv[1]);
     int * arr = new int [n];
     
-    generateReduceData (arr, atoi(argv[1]));
-    
     // insert reduction code here
     
     int chunkSize = atoi(argv[4]);
 
-    int * arr = new int [atoi(argv[1])];
-
-    generateReduceData (arr, atoi(argv[1]));
+    generateReduceData (arr, n);
 
     struct timeval start, end;
-
+    std::string sched_type = argv[3];
+    //std::cout<<scheduling_type<<std::endl;
 
     int result = arr[0];
-    if(staticStr.compare(argv[3])==0){
+    if(staticStr.compare(sched_type)==0){
         omp_set_schedule(omp_sched_static, chunkSize);
     }
-    else if(dynamicStr.compare(argv[3])==0){
+    else if(dynamicStr.compare(sched_type)==0){
         omp_set_schedule(omp_sched_dynamic, chunkSize);
     }
     else
@@ -73,16 +70,23 @@ int main (int argc, char* argv[]) {
 
 
 #pragma omp for schedule(runtime)
-        for(int i=1; i < atoi(argv[1]); i++ )
+        for(int i=1; i < n; i++ )
             result += arr[i];
     }
     gettimeofday(&end, NULL);
+    
 
-    double last_time = (end.tv_sec – start.tv_sec + (end.tv_usec - start.tv_usec)/1000000); // in seconds
+    double s_sec=start.tv_sec;
+    double e_sec=end.tv_sec;
+    double diff_sec= e_sec-s_sec;
+    double s_usec=start.tv_usec;
+    double e_usec=end.tv_usec;
+    double diff_usec=(e_usec-s_usec)/1000000;
+    double last_time= diff_sec+ diff_usec;
 
-    std::cerr<<<<std::endl;
+     std::cerr<<last_time<<std::endl;
     
     delete[] arr;
-    
+    std::cout<<result<<std::endl; 
     return 0;
 }
