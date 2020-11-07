279136411d4157e78124ea30ea8029aa6d35dec0
Manish Ladkat <manishladkat@Manishs-MacBook-Pro.local>
2020-11-07 06:18:07 +0530

    lcs code

--- assignment-openmp-task/lcs/lcs.cpp
+++ assignment-openmp-task/lcs/lcs.cpp
@@ -92,7 +92,7 @@ int main (int argc, char* argv[]) {
                 if(n<=10)
                     granularity = 50;
                 else 
-                    granularity += 5*n*0.01;
+                    granularity += 5*n;
 
 
                 if (X[k-1] == Y[k-1]){
